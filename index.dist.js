b9c741c718147ace2d0bfc59df3a1623984ffb00
Logan Barnett <logustus@gmail.com>
2020-04-20 22:31:46 -0700

    fix misc upgrade issues and prepare for 0.2.0

--- index.dist.js
+++ index.dist.js
@@ -1,13 +1,7 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-exports.default = function (f) {
+export default (f => {
   try {
     return f();
   } catch (e) {
     return e instanceof Error ? e : new Error(e);
   }
-};
+});
